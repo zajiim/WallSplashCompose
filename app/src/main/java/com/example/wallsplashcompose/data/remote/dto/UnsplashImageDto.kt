7cd774ae5fbb9af69cabc6be21c276dedeb19668
zajiim <sajims95@gmail.com>
2024-07-06 10:28:01 +0530

    Added dto

--- app/src/main/java/com/example/wallsplashcompose/data/remote/dto/UnsplashImageDto.kt
+++ app/src/main/java/com/example/wallsplashcompose/data/remote/dto/UnsplashImageDto.kt
@@ -6,32 +6,56 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class UnsplashImageDto(
+    @SerialName("id")
+    val id: String,
     @SerialName("blur_hash")
     val blurHash: String,
     @SerialName("color")
     val color: String,
-    @SerialName("created_at")
-    val createdAt: String,
-    @SerialName("current_user_collections")
-    val currentUserCollections: List<CurrentUserCollection>,
     @SerialName("description")
-    val description: String,
+    val description: String?,
     @SerialName("height")
     val height: Int,
-    @SerialName("id")
-    val id: String,
-    @SerialName("liked_by_user")
-    val likedByUser: Boolean,
-    @SerialName("likes")
-    val likes: Int,
-    @SerialName("links")
-    val links: Links,
-    @SerialName("updated_at")
-    val updatedAt: String,
     @SerialName("urls")
     val urls: Urls,
     @SerialName("user")
     val user: User,
     @SerialName("width")
     val width: Int
+)
+
+@Serializable
+data class Urls(
+    @SerialName("full")
+    val full: String,
+    @SerialName("raw")
+    val raw: String,
+    @SerialName("regular")
+    val regular: String,
+    @SerialName("small")
+    val small: String,
+    @SerialName("thumb")
+    val thumb: String
+)
+
+@Serializable
+data class User(
+    @SerialName("links")
+    val links: LinksX,
+    @SerialName("name")
+    val name: String,
+    @SerialName("portfolio_url")
+    val portfolioUrl: String,
+    @SerialName("profile_image")
+    val profileImage: ProfileImage,
+    @SerialName("total_collections")
+    val totalCollections: Int,
+    @SerialName("total_likes")
+    val totalLikes: Int,
+    @SerialName("total_photos")
+    val totalPhotos: Int,
+    @SerialName("twitter_username")
+    val twitterUsername: String,
+    @SerialName("username")
+    val username: String
 )
