6272e4af59eb3d17af70df7ad6e2861443678031
zajiim <sajims95@gmail.com>
2024-07-09 00:40:10 +0530

    Added splash screen

--- app/src/main/java/com/example/wallsplashcompose/MainActivity.kt
+++ app/src/main/java/com/example/wallsplashcompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.wallsplashcompose.presentation.main.MainScreen
 import com.example.wallsplashcompose.presentation.ui.theme.WallSplashComposeTheme
 
@@ -11,6 +12,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             WallSplashComposeTheme {
                 MainScreen()
