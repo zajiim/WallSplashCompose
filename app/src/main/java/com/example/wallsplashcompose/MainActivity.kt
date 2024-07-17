b7fb568c198b9ca2b2896e7c878bfd573fcea8a8
zajiim <sajims95@gmail.com>
2024-07-17 12:44:12 +0530

    Changes in status bar and navigationbar color in both modes..

--- app/src/main/java/com/example/wallsplashcompose/MainActivity.kt
+++ app/src/main/java/com/example/wallsplashcompose/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.wallsplashcompose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.wallsplashcompose.presentation.main.MainScreen
 import com.example.wallsplashcompose.presentation.ui.theme.WallSplashComposeTheme
@@ -13,7 +16,16 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(
+                lightScrim = Color.Transparent.toArgb(),
+                darkScrim = Color.Transparent.toArgb()
+            ),
+            navigationBarStyle = SystemBarStyle.auto(
+                lightScrim = Color.Transparent.toArgb(),
+                darkScrim = Color.Transparent.toArgb()
+            )
+        )
         installSplashScreen()
         setContent {
             WallSplashComposeTheme {
--- app/src/main/java/com/example/wallsplashcompose/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/wallsplashcompose/presentation/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.example.wallsplashcompose.presentation.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,12 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
@@ -86,6 +92,16 @@ fun WallSplashComposeTheme(
 
         darkTheme -> darkScheme
         else -> lightScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        DisposableEffect(darkTheme) {
+            val window = (view.context as Activity).window
+            window.statusBarColor = if (darkTheme) backgroundDark.toArgb() else backgroundLight.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            onDispose {}
+        }
     }
 
     MaterialTheme(
